e8284256f2f1c199541bea70783d99db4778f178
LeeChungWoo <ios.lllccww@gmail.com>
2023-04-06 23:54:29 +0900

    Modify: 라이브러리 충돌로 인한 프로젝트 모듈 이름 변경

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:video_player/screen/home_screen.dart';
+import 'package:video_player_example/screen/home_screen.dart';
 
 void main() {
   runApp(
