da6e6d42288ab4adeff0d66f90d69f1d4f10ee92
LeeChungWoo <ios.lllccww@gmail.com>
2023-04-10 23:49:03 +0900

    Add: CustomVideoPlayer component

---
+++ lib/component/custom_video_player.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:video_player/video_player.dart';
+
+class CustomVideoPlayer extends StatefulWidget {
+  final XFile video;
+
+  const CustomVideoPlayer({required this.video, Key? key}) : super(key: key);
+
+  @override
+  State<CustomVideoPlayer> createState() => _CustomVideoPlayerState();
+}
+
+class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
+  VideoPlayerController? videoPlayerController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
