7d3ed0995d2ae88edd9b36c1eabbe56e89a4e1b0
LeeChungWoo <ios.lllccww@gmail.com>
2023-04-11 23:01:08 +0900

    Add: 재생관련 버튼 추가

--- lib/component/custom_video_player.dart
+++ lib/component/custom_video_player.dart
@@ -35,13 +35,61 @@ class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
 
   @override
   Widget build(BuildContext context) {
-    if(videoPlayerController == null){
+    if (videoPlayerController == null) {
       return CircularProgressIndicator();
     }
     return AspectRatio(
       aspectRatio: videoPlayerController!.value.aspectRatio,
-      child: VideoPlayer(
-        videoPlayerController!,
+      child: Stack(
+        children: [
+          VideoPlayer(
+            videoPlayerController!,
+          ),
+          _Controls()
+        ],
+      ),
+    );
+  }
+}
+
+class _Controls extends StatelessWidget {
+  const _Controls({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black.withOpacity(0.5),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          rederIconButton(
+            onPressed: () {},
+            iconData: Icons.rotate_left,
+          ),
+          rederIconButton(
+            onPressed: () {},
+            iconData: Icons.play_arrow,
+          ),
+          rederIconButton(
+            onPressed: () {},
+            iconData: Icons.rotate_right,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget rederIconButton({
+    required VoidCallback onPressed,
+    required IconData iconData,
+  }) {
+    return IconButton(
+      onPressed: onPressed,
+      iconSize: 30.0,
+      color: Colors.white,
+      icon: Icon(
+        Icons.play_arrow,
       ),
     );
   }
